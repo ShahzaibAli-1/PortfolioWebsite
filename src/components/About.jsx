a23e2b7325454b3536ecc9a0afddc5dac38172b7
Shahzaib Ali <sa7979798@gmail.com>
2025-04-20 23:18:45 +0500

    changed react-tilt to react parallax tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,26 +8,24 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt
+    tiltMaxAngleX={45}
+    tiltMaxAngleY={45}
+    scale={1}
+    transitionSpeed={450}
+    className="xs:w-[250px] w-full"
+  >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
+      <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
         <img
           src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
+          alt="web-development"
+          className="w-16 h-16 object-contain"
         />
-
-        <h3 className='text-white text-[20px] font-bold text-center'>
+        <h3 className="text-white text-[20px] font-bold text-center">
           {title}
         </h3>
       </div>
@@ -45,14 +43,19 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-I'm a versatile software engineer specializing in AI/ML and full-stack development. With hands-on experience in building intelligent systems and a solid foundation in TypeScript, JavaScript, and frameworks like React, Node.js, and Three.js, I bring machine learning solutions to life—from model testing and evaluation to seamless integration into scalable applications. I'm passionate about developing efficient, data-driven products that solve real-world problems. Let's build smart, impactful technology together!
-
-
+        I'm a versatile software engineer specializing in AI/ML and full-stack
+        development. With hands-on experience in building intelligent systems
+        and a solid foundation in TypeScript, JavaScript, and frameworks like
+        React, Node.js, and Three.js, I bring machine learning solutions to
+        life—from model testing and evaluation to seamless integration into
+        scalable applications. I'm passionate about developing efficient,
+        data-driven products that solve real-world problems. Let's build smart,
+        impactful technology together!
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
